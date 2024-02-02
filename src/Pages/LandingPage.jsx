7dcf7d0a6b65ef2f8a8c6f60bba4e446e420c8e6
quinnasher <pauldivineagionoja@gmail.com>
2024-02-02 18:20:45 +0100

    added background and removed overflow

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,35 +1,31 @@
 import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
-import OurService from "../components/OurService.jsx";
-import ourServiceData from "../data/ourServiceData.js";
-import testimonialData from "../data/testimonialData.js";
+import Value from "../components/Value.jsx";
+import values from "../data/values.js";
+import testimonialData from "../data/testimonials.js";
 import Testimonial from "../components/Testimonial.jsx";
 
 function LandingPage() {
   return (
-    <div className={" relative mx-auto mt-4 w-375 md:w-1300"}>
-      {/*  Absolute positioning is used to add background images to follow the design. A more
-       efficient way might be available, but this is the method I can implement*/}
-      {/*<div*/}
-      {/*  className={*/}
-      {/*    "bg-tablet-pattern md:w-450px absolute -right-8 -top-32 -z-40 h-[400px] w-[340px] bg-cover bg-left bg-no-repeat md:-right-0 md:-top-52 md:h-[450px]"*/}
-      {/*  }*/}
-      {/*></div>{" "}*/}
-      {/*<div*/}
-      {/*  className={*/}
-      {/*    "bg-tablet-pattern absolute -right-40 top-[30rem] -z-50 h-[400px] w-[340px] bg-cover bg-left bg-no-repeat md:-left-36 md:bottom-52 md:h-[450px] md:w-450"*/}
-      {/*  }*/}
-      {/*></div>*/}
+    <div className={" relative mx-auto mt-4 w-375 overflow-hidden md:w-1300"}>
+      {/*Absolute positioning is used to add background images to follow the*/}
+      {/*design. A more efficient way might be available, but this is the method I*/}
+      {/*can implement*/}
+      <div
+        className={
+          "bg-tablet-pattern md:w-450px absolute -right-8 -top-32 -z-40 h-[400px] w-[340px] bg-cover bg-left bg-no-repeat md:-right-0 md:-top-52 md:h-[450px]"
+        }
+      ></div>{" "}
+      <div
+        className={
+          "bg-tablet-pattern absolute -right-40 top-[30rem] -z-50 h-[400px] w-[340px] bg-cover bg-left bg-no-repeat md:-left-36 md:bottom-52 md:h-[450px] md:w-450"
+        }
+      ></div>
       <div className={"mx-auto w-340 md:w-1100"}>
         <Header />
 
         {/*    Services section*/}
-        {ourServiceData.map((service, index) => (
-          // eslint-disable-next-line react/jsx-key
-          <div className={index !== ourServiceData.length - 1 ? "mb-9 " : ""}>
-            <OurService service={service} />
-          </div>
-        ))}
+        <Value values={values} />
       </div>
       <div>
         <Testimonial testimonial={testimonialData} />
---
+++ src/components/Value.jsx
@@ -0,0 +1,40 @@
+function Value(props) {
+  return (
+    <div>
+      {/* eslint-disable-next-line react/prop-types */}
+      {props.values.map((value, index) => (
+        // eslint-disable-next-line react/jsx-key
+        // <div className={index !== props.values.length - 1 ? "mb-9 " : ""}>
+        //     <Value service={service}/>
+        // </div>
+
+        <section
+          key={index}
+          className={"relative ml-auto flex w-360 gap-12 md:mx-0 md:ml-auto"}
+        >
+          <span className={"btn absolute md:-left-20 md:top-0"}>
+            {value.number}
+          </span>
+
+          <div className={"flex flex-col gap-3  "}>
+            <div className={"rounded-l-3xl bg-veryPaleRed md:bg-transparent"}>
+              <h2 className={"ml-18h  w-300 py-2  font-700 md:ml-0"}>
+                {value.tittle}
+              </h2>
+            </div>
+
+            <p
+              className={
+                "mr-2 leading-loose tracking-wide text-darkGrayishBlue md:mr-0"
+              }
+            >
+              {value.content}
+            </p>
+          </div>
+        </section>
+      ))}
+    </div>
+  );
+}
+
+export default Value;
