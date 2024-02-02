07b1c573f185221b6b2d26d8bdf82378db6eb2ae
quinnasher <pauldivineagionoja@gmail.com>
2024-02-02 09:34:14 +0100

    removed background images as it disrupted the page's display

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -10,16 +10,16 @@ function LandingPage() {
     <div className={" relative mx-auto mt-4 w-375 md:w-1300"}>
       {/*  Absolute positioning is used to add background images to follow the design. A more
        efficient way might be available, but this is the method I can implement*/}
-      <div
-        className={
-          "bg-tablet-pattern md:w-450px absolute -right-8 -top-32 -z-40 h-[400px] w-[340px] bg-cover bg-left bg-no-repeat md:-right-0 md:-top-52 md:h-[450px]"
-        }
-      ></div>{" "}
-      <div
-        className={
-          "bg-tablet-pattern absolute -right-40 top-[30rem] -z-50 h-[400px] w-[340px] bg-cover bg-left bg-no-repeat md:-left-36 md:bottom-52 md:h-[450px] md:w-450"
-        }
-      ></div>
+      {/*<div*/}
+      {/*  className={*/}
+      {/*    "bg-tablet-pattern md:w-450px absolute -right-8 -top-32 -z-40 h-[400px] w-[340px] bg-cover bg-left bg-no-repeat md:-right-0 md:-top-52 md:h-[450px]"*/}
+      {/*  }*/}
+      {/*></div>{" "}*/}
+      {/*<div*/}
+      {/*  className={*/}
+      {/*    "bg-tablet-pattern absolute -right-40 top-[30rem] -z-50 h-[400px] w-[340px] bg-cover bg-left bg-no-repeat md:-left-36 md:bottom-52 md:h-[450px] md:w-450"*/}
+      {/*  }*/}
+      {/*></div>*/}
       <div className={"mx-auto w-340 md:w-1100"}>
         <Header />
 
--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,15 +1,38 @@
-function Testimonial(props) {
-  function previousImage() {}
+import { useEffect, useState } from "react";
 
-  function nextImage() {}
+function Testimonial(props) {
+  const [testimonialIndex, setTestimonialIndex] = useState(0);
+  function previousTestimonial() {
+    setTestimonialIndex((index) => {
+      // eslint-disable-next-line react/prop-types
+      if (testimonialIndex === 0) return props.testimonial.length - 1;
+      else return testimonialIndex - 1;
+    });
+  }
+
+  function nextTestimonial() {
+    setTestimonialIndex((index) => {
+      if (testimonialIndex === props.testimonial.length - 1) return 0;
+      else return testimonialIndex - 1;
+    });
+  }
+
+  // Set up an interval to change the testimonial every 3 seconds
+  useEffect(() => {
+    const interval = setInterval(() => {
+      nextTestimonial();
+    }, 3000);
+    // Clear the interval when the component unmounts or the index changes
+    return () => clearInterval(interval);
+  }, [testimonialIndex]);
 
   return (
-    <div>
+    <div className={"flex flex-col  gap-12 md:flex-row"}>
       {/* eslint-disable-next-line react/prop-types */}
       {props.testimonial.map((testimonial) => (
         <section
           className={
-            "md:w-400 relative mx-auto mb-8 flex w-360 flex-col items-center justify-center bg-VeryLightGray px-6 py-12"
+            "md:w-400 relative mx-auto flex w-360 flex-col items-center justify-center bg-VeryLightGray px-6 py-12"
           }
           key={testimonial.id}
         >
@@ -20,7 +43,11 @@ function Testimonial(props) {
           />
           <h2 className={"mb-6 font-700"}>{testimonial.name}</h2>
           <div>
-            <p className={"text-center leading-relaxed tracking-normal"}>
+            <p
+              className={
+                "text-center leading-normal tracking-normal text-darkGrayishBlue"
+              }
+            >
               {testimonial.testimony}
             </p>
           </div>
